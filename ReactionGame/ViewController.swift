bfe674939b319529374e9ee7b0bacb90298b0af3
HsinAn Lin <hsinan.lin817@gmail.com>
2023-02-13 16:57:18 +0800

    1. fixed the frame where letters can appear / 2. added the function to display best & worst records / 3. added comments

--- ReactionGame/ViewController.swift
+++ ReactionGame/ViewController.swift
@@ -4,16 +4,21 @@
 //
 //  Created by Lin Hsin-An on 2023/2/10.
 //
+//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var instruction: UIStackView!
     @IBOutlet weak var timeCount: UILabel!
     @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet weak var popupButton: UIButton!
+    @IBOutlet weak var restartButton: UIButton!
     
     var counter = 0.0
+    var worstTime = 0.0
+    var bestTime = 1000000000.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +27,7 @@ class ViewController: UIViewController {
         // Set up a timer (tie it to the function updateCounter)
         Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         
-        // Hide popup letters
+        // Hide the popup letter
         popupButton.isHidden = true
         
     }
@@ -30,51 +35,68 @@ class ViewController: UIViewController {
     @objc func updateCounter() {
         // Display time passed when game starts
         if newGameButton.isHidden && !popupButton.isHidden{
-            timeCount.text = "\(counter/100)s" //String(format:"%.2fs", counter/100)
+            var timePassed = counter/100
+            
+            // Display time passed
+            timeCount.text = "\(timePassed)s"
+            
             counter += 1
         }else{
+            // Make time count back to 0 when game restarts or when a letter is tapped
             counter = 0.0
+        }
+    }
+    func recordTime(){
+        //Store time records
+        var timePassed = counter/100
+        if timePassed > worstTime{
+            worstTime = timePassed
+        }
+        if timePassed < bestTime && timePassed > 0{
+            bestTime = timePassed
         }
     }
     
     func changeLetter(){
-        //choose a random letter
-        let arr = Array(arrayLiteral: "軟", "體", "工", "程")
-        let randomNum = Int.random(in: 0...arr.count-1)
-        //popupButton.titleLabel?.text = arr[randomNum]
-        popupButton.setTitle(arr[randomNum], for: .normal)
+        //Generate a random letter to display
+        ////  1. Create an array of English Alphabets
+        let aScalars = "a".unicodeScalars
+        let aCode = aScalars[aScalars.startIndex].value
+        let letters: [Character] = (0..<26).map {
+            i in Character(UnicodeScalar(aCode + i) ?? "a")
+        }
+        //// 2. Generate a random letter to display
+        let randomNum = Int.random(in: 0...letters.count-1)
+        let randomLetter = String(letters[randomNum])
+        popupButton.setTitle(randomLetter, for: .normal)
         
-        //*change where the letter is positioned
+        //Change where the letter is positioned
+        //// 1. Get element coordinates and Set frame limit
+        let letterFrame_x_left = 0.0
+        let letterFrame_x_right = timeCount.frame.origin.x
+        let letterFrame_y_top = instruction.frame.origin.y + 50
+        let letterFrame_y_bottom = restartButton.frame.origin.y - restartButton.frame.size.height - 50
+        //// 2. Set letter's coordinates randomly based on frame limit
         popupButton.frame.origin = CGPoint(
-            x: Int.random(in: 0...200),
-            y: Int.random(in: 100...600)
+            x: CGFloat.random(in: letterFrame_x_left...letterFrame_x_right), //Int.random(in: 0...200),
+            y: CGFloat.random(in: letterFrame_y_top...letterFrame_y_bottom) //Int.random(in: 100...600)
         )
         
-        //*change letter size
-        /*
-        popupButton.frame.size = CGSize(
-            width: Double.random(in: 20...200),
-            height: Double.random(in: 20...200)
-        )
-        */
+        //Change letter size
         popupButton.titleLabel?.font = UIFont(
             name: "Noto Sans Kannada",
-            size: CGFloat.random(in: 5...75)
+            size: CGFloat.random(in: 5...30)
         )
-        //popupButton.titleLabel?.font.withSize(CGFloat.random(in: 5...75))
         
-        //change letter color
+        //Change letter color
         popupButton.setTitleColor(UIColor(
             red: Double.random(in: 0...1),
             green: Double.random(in: 0...1),
             blue: Double.random(in: 0...1),
-            alpha: Double.random(in: 0.5...1)), for: .normal
+            alpha: Double.random(in: 0.2...0.6)), for: .normal
         )
         
-        //*change letter angle
-        
-        //show the letter
-        //popupButton.isHidden = false
+        //Display the letter
         let delay = Double.random(in: 0...2)
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
             self.popupButton.isHidden = false
@@ -83,21 +105,41 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startGame(_ sender: UIButton) {
+        // Hide "new game" button
         newGameButton.isHidden = true
+        // Display a new letter
         changeLetter()
     }
     
     @IBAction func letterTapped(_ sender: UIButton) {
+        // Hide the tapped letter
         popupButton.isHidden = true
-        changeLetter()        
+        // Store records
+        recordTime()
+        // Display a new letter
+        changeLetter()
     }
     
-    //Record best record
-    
     @IBAction func restartGame(_ sender: UIButton) {
-        newGameButton.isHidden = false
+        // Hide the letter
         popupButton.isHidden = true
-        timeCount.text = "0s"
+        
+        // Announce records
+        let recordAnnouncement = UIAlertController(
+            title: "Your best and worst records are:",
+            message: "Best record: \(bestTime)s \nWorst record: \(worstTime)s",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil)
+        recordAnnouncement.addAction(okAction)
+        present(recordAnnouncement, animated: true)
+        
+        // Show new game button
+        newGameButton.isHidden = false
+        
+        
     }
 }
 
